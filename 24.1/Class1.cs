335c84ac7a653846a7bc359f521af78fb582da32
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-24 19:01:13 +0200

    Add files via upload

---
+++ 24.1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Avto
+    {
+ 
+        string brand;
+        string color;
+        double skor;
+
+        public string Brand { get { return brand; } set { brand = value; } }
+        public string Color { get { return color; } set { color = value; } }
+        public double Skor { get { return skor; } set { if (value < 20) { skor = 20; } else if (value > 120) { skor = 120; } else { skor = value; } } }
+ 
+        public Avto() { }
+ 
+        public Avto(string brand, string color, double skor)
+        {
+            this.Brand = brand;
+            this.Color = color;
+            this.Skor = skor;
+        }
+        public override string ToString()
+        {
+            return $"Brand = {Brand}, Color = {Color}, Skor = {Skor}";
+        }
+    }
+}
---
+++ 24.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Avto avto = new Avto();
+            Console.WriteLine(avto.ToString());
+           
+            Avto avto2 = new Avto("БМВ", "Желтый", 220);
+            Console.WriteLine(avto2.ToString());
+           
+            Console.WriteLine("Введите марку");
+            string brand = Console.ReadLine();
+            Console.WriteLine("Введите цвет");
+            string color = Console.ReadLine();
+            Console.WriteLine("Введите скорость");
+            double skor = double.Parse(Console.ReadLine());
+            Avto avto3 = new Avto(brand, color, skor);
+            Console.WriteLine($"{avto3.ToString()}\n");
+            Console.ReadLine();
+        }
+    }
+}
