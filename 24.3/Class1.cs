3b5f7c09ea6aa41907406e915219abd00ab048bf
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-24 19:00:44 +0200

    Add files via upload

---
+++ 24.3/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._3
+{
+    internal class Kadry
+    {
+        private string fam;
+        private int age;
+        private string dol;
+        private int staj;
+
+        public Kadry()
+        {
+            fam = "";
+            age = 16;
+            dol = "";
+            staj = 0;
+        }
+
+        public Kadry(string fam, int age, string dol, int staj)
+        {
+            this.Fam = fam;
+            this.Age = age;
+            this.Dol = dol;
+            this.Staj = staj;
+        }
+
+        public string Fam
+        {
+            get { return fam; }
+            set { fam = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 16 && value <= 60)
+                    age = value;
+                else
+                    throw new ArgumentException("Возраст должен быть в пределах от 16 до 60 лет.");
+            }
+        }
+
+        public string Dol
+        {
+            get { return dol; }
+            set { dol = value; }
+        }
+
+        public int Staj
+        {
+            get { return staj; }
+            set
+            {
+                if (value >= 0 && value <= 45)
+                    staj = value;
+                else
+                    throw new ArgumentException("Стаж должен быть в пределах от 0 до 45 лет.");
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Фамилия: {fam}, Возраст: {age}, Должность: {dol}, Стаж: {staj}");
+        }
+    }
+
+}
+
---
+++ 24.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Kadry kadry1 = new Kadry();
+            kadry1.Fam = "Иванов";
+            kadry1.Age = 30;
+            kadry1.Dol = "Инженер";
+            kadry1.Staj = 10;
+            kadry1.Display();
+
+            Kadry kadry2 = new Kadry("Петров", 40, "Менеджер", 15);
+            kadry2.Display();
+
+            Console.Write("Введите фамилию: ");
+            string fam = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Введите должность: ");
+            string dol = Console.ReadLine();
+            Console.Write("Введите стаж: ");
+            int staj = int.Parse(Console.ReadLine());
+
+            Kadry kadry3 = new Kadry(fam, age, dol, staj);
+            kadry3.Display();
+        }
+    }
+}
